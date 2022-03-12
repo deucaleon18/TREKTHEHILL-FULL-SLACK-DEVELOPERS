ac9880e522e6f623ea4236e4d9f1cc76a7ea585d
deucaleon18 <shubham37908@gmail.com>
2022-03-13 00:26:42 +0530

    DEPENDANCIES CHANGED

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -22,56 +22,56 @@ const Register = () => {
 
 
 //   Register handler
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        await client
-          .add(buffer)
-          .then(async (res) => {
-            console.log(res.path)
-            setImageHash(res.path);
-                await contract.methods
-                  .createUser(email, username, paymentAccount)
-                  .send({ from: account })
-                  .then(async(res) => {
-                    console.log(res);
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     await client
+    //       .add(buffer)
+    //       .then(async (res) => {
+    //         console.log(res.path)
+    //         setImageHash(res.path);
+    //             await contract.methods
+    //               .createUser(email, username, paymentAccount)
+    //               .send({ from: account })
+    //               .then(async(res) => {
+    //                 console.log(res);
                     
-                    // eslint-disable-next-line no-console
-                    await axios
-                      .post("http://localhost:5000/register/", {
-                        username,
-                        email,
-                        password,
-                        paymentAccount,
-                        imageHash
-                      })
-                      .then((res) => {
-                        console.log(res);
-                        localStorage.setItem("token", res.data.token);
-                        localStorage.setItem("username", res.data.user.username);
-                        localStorage.setItem("email", res.data.user.email);
-                        localStorage.setItem(
-                          "paymentAccount",
-                          res.data.user.paymentAccount
-                        );
-                      })
-                      .then(() => {
-                        window.location.href = "/app";
-                      })
-                      .catch((err) => {
-                        console.log(err);
-                      });
-                  })
+    //                 // eslint-disable-next-line no-console
+    //                 await axios
+    //                   .post("http://localhost:5000/register/", {
+    //                     username,
+    //                     email,
+    //                     password,
+    //                     paymentAccount,
+    //                     imageHash
+    //                   })
+    //                   .then((res) => {
+    //                     console.log(res);
+    //                     localStorage.setItem("token", res.data.token);
+    //                     localStorage.setItem("username", res.data.user.username);
+    //                     localStorage.setItem("email", res.data.user.email);
+    //                     localStorage.setItem(
+    //                       "paymentAccount",
+    //                       res.data.user.paymentAccount
+    //                     );
+    //                   })
+    //                   .then(() => {
+    //                     window.location.href = "/app";
+    //                   })
+    //                   .catch((err) => {
+    //                     console.log(err);
+    //                   });
+    //               })
     
-                  .catch((err) => {
-                    console.log(err);
-                  });
-                })
-                .catch((err)=>{
-                  console.log(err)
+    //               .catch((err) => {
+    //                 console.log(err);
+    //               });
+    //             })
+    //             .catch((err)=>{
+    //               console.log(err)
     
-                })
+    //             })
                 
-      };
+    //   };
 
 
       
